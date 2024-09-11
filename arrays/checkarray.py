c9c486b1a14495b997d1664803cefb85bb526a62
Udit <uditrawat1905@gmail.com>
2024-09-11 23:41:10 +0530

    daily push

---
+++ arrays/checkarray.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        count = 0
+        for i in range(1, n):
+            if nums[i] < nums[i - 1]:
+                count += 1
+
+        if nums[-1] > nums[0]:
+            count += 1
+
+        return count <= 1

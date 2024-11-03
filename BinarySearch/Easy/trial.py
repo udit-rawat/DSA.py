48e87b258e5c74f0a0d7cb01ffc04be86c183ce3
Udit <uditrawat1905@gmail.com>
2024-11-04 03:58:36 +0530

    add

---
+++ BinarySearch/Easy/trial.py
@@ -0,0 +1,10 @@
+s = [4, 5, 6, 7, 0, 1, 2]
+
+og = sorted(s)
+# rotation = og.index(max(og)) - nums.index(max(nums)) + 1
+# print(rotation)
+# print(nums[rotation])
+
+target = s[len(s)-1]
+print(type(target))
+print(3 < 3)
